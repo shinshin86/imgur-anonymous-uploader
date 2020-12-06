f5a4616fada3a20cf63ea97c920b4630aac9756d
shinshin86 <beagles1986@gmail.com>
2020-12-06 18:52:13 +0900

    Add a comment about uploadFile ()

--- examples/upload.js
+++ examples/upload.js
@@ -8,6 +8,12 @@ const path = require('path');
     const uploader = new ImgurAnonymousUploader(process.env.IMGUR_CLIENT_ID);
     const response = await uploader.upload(imagePath);
 
+    /**
+     * or use uploadFile() method (which just calls upload()).
+     * This was added for method name consistency.
+     */
+    // const response = await uploader.uploadFile(imagePath);
+
     console.log(response);
   } catch (error) {
     console.error(error);
